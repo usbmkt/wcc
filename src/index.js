781ed9c6e9174c06f5879eadf2a32e175440fab7
usbmkt <usbmktpro@gmail.com>
2025-09-27 03:21:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Define a variável CSS para a imagem de fundo
+document.documentElement.style.setProperty('--background-image', `url(${process.env.PUBLIC_URL}/background.jpg)`);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
